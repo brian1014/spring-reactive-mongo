04c86c3274bd3fe75b4bfa5925366d104a9e656d
Brian Alvarez <brianalvarez2000@gmail.com>
2026-08-29 19:22:33 -0500

    Section 32: Spring WebFlux.fn Rest Services

---
+++ src/main/java/guru/springframework/springreactivemongo/services/CustomerServiceImpl.java
@@ -0,0 +1,69 @@
+package guru.springframework.springreactivemongo.services;
+
+import guru.springframework.springreactivemongo.mappers.CustomerMapper;
+import guru.springframework.springreactivemongo.model.CustomerDTO;
+import guru.springframework.springreactivemongo.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public Flux<CustomerDTO> listCustomers() {
+        return customerRepository.findAll()
+                .map(customerMapper::entityToDto);
+    }
+
+    @Override
+    public Mono<CustomerDTO> getCustomerById(String customerId) {
+        return customerRepository.findById(customerId)
+                .map(customerMapper::entityToDto);
+    }
+
+    @Override
+    public Mono<CustomerDTO> saveNewCustomer(CustomerDTO customerDTO) {
+        return customerRepository.save(customerMapper.dtoToEntity(customerDTO))
+                .map(customerMapper::entityToDto);
+    }
+
+    @Override
+    public Mono<CustomerDTO> saveNewCustomer(Mono<CustomerDTO> customerDTO) {
+        return customerDTO.map(customerMapper::dtoToEntity)
+                .flatMap(customerRepository::save)
+                .map(customerMapper::entityToDto);
+    }
+
+    @Override
+    public Mono<CustomerDTO> updateCustomer(String customerId, CustomerDTO customerDTO) {
+        return customerRepository.findById(customerId)
+                .map(customer -> {
+                    customer.setCustomerName(customerDTO.getCustomerName());
+                    return customer;
+                }).flatMap(customerRepository::save)
+                .map(customerMapper::entityToDto);
+    }
+
+    @Override
+    public Mono<CustomerDTO> patchCustomer(String customerId, CustomerDTO customerDTO) {
+        return customerRepository.findById(customerId)
+                .map(customer -> {
+                    if (StringUtils.hasText(customerDTO.getCustomerName())){
+                        customer.setCustomerName(customerDTO.getCustomerName());
+                    }
+                    return customer;
+                }).flatMap(customerRepository::save)
+                .map(customerMapper::entityToDto);
+    }
+
+    @Override
+    public Mono<Void> deleteCustomerById(String customerId) {
+        return customerRepository.deleteById(customerId);
+    }
+}
